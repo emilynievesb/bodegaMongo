07353e929ab657aa92c3ba99f5fa6566aedbca38
emilynievesb <nemily1710@gmail.com>
2023-08-13 13:39:14 -0500

    refactor: :recycle: Refactor función de conexión
    Se realizó refactor de la función de conexión, para poder reutilizar la conexión y no limitarla a consultas de colección

--- src/utils/connect.js
+++ src/utils/connect.js
@@ -1,7 +1,7 @@
 import { db } from "./config.js";
 import { MongoClient } from "mongodb";
 
-export default async function connection(col) {
+export async function connect() {
   try {
     const url = `mongodb+srv://${db.user}:${db.pass}@cluster0.rhimngz.mongodb.net/${db.dbname}`;
     const options = {
@@ -10,7 +10,15 @@ export default async function connection(col) {
     };
     const client = await MongoClient.connect(url, options);
     const data = client.db();
-    const res = data.collection(col);
+    return data;
+  } catch (error) {
+    return { status: 500, message: error };
+  }
+}
+export default async function connection(col) {
+  try {
+    const db = await connect();
+    const res = db.collection(col);
     return res;
   } catch (error) {
     return { status: 500, message: error };
