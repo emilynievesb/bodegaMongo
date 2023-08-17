651b5ec7932c864d64ac9033c5f4267fe1b7ac54
emilynievesb <nemily1710@gmail.com>
2023-08-17 06:32:56 -0500

    fix: :sparkles: Error parámetro
    Se arreglo error de un parámetro que no estaba siendo usado

--- src/controllers/postControllers.js
+++ src/controllers/postControllers.js
@@ -8,12 +8,7 @@ import {
 const agregarBodegaController = async (req, res, next) => {
   try {
     const { nombre, id_responsable, estado, creador } = req.body;
-    const bodega = await agregarBodega(
-      nombre,
-      id_responsable,
-      estado,
-      created_by
-    );
+    const bodega = await agregarBodega(nombre, id_responsable, estado, creador);
     res.status(200).json(bodega);
   } catch (error) {
     res.status(500).json(error.message);
